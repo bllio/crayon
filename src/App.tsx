e0111f874d21db6901b57fd9c9e034bf920c1846
bllio <89642563+bllio@users.noreply.github.com>
2025-10-14 23:08:05 +0800

    Animate Palette on exit and entry

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import Logo from './components/Logo/Logo';
 import Palette from './components/Palette/Palette';
 import Toolbar from './components/Toolbar/Toolbar';
 
+import { useAppStore } from './stores/useAppStore';
+
 import styles from './App.module.css';
 
 export default function App() {
+  const activeTool = useAppStore((state) => state.activeTool);
+
   return (
     <>
       <Logo />
       <div className={styles.layout}>
-        <div>
+        <div
+          className={`${styles.layout__left} ${activeTool !== 0 ? styles['layout__left--hidden'] : ''}`}
+        >
           <Palette />
         </div>
-        <div>
+        <div className={styles.layout__center}>
           <Canvas />
         </div>
         <div>
